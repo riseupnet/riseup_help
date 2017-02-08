f287bd4e920008c5856137590e91972906decb74
elijah <elijah@riseup.net>
2017-02-08 15:34:42 -0800

    enable hindi

--- amber/config.rb
+++ amber/config.rb
@@ -1,4 +1,4 @@
 @title = "riseup.net"
 @default_locale = :en
-@locales = [:zh, :es, :en, :pt, :ru, :de, :fr, :it, :pl, :el, :ca]
+@locales = [:zh, :es, :en, :pt, :ru, :de, :fr, :it, :pl, :el, :ca, :hi]
 @short_paths = true
